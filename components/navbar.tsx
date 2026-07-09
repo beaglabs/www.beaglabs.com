2ee973e137a348f3f2166c2bbbc8dcbbbcb45259
James Bohrman <james@getoasis.io>
2026-07-09 05:48:17 -0400

    push

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -21,10 +21,10 @@ export function Navbar({ bannerHeight = 0 }: { bannerHeight?: number }) {
             Models
           </Link>
           <Link
-            href="#capabilities"
+            href="/blog"
             className="text-[11px] font-bold uppercase tracking-[0.1em] text-[#111] border-b-2 border-transparent transition-all hover:border-[#FF5F1F]"
           >
-            Capabilities
+            Blog
           </Link>
           <Link
             href="/cookbook"
