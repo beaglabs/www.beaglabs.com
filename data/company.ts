2ad29f6258016261ad7f98de43895e4200d23455
James Bohrman <james@getoasis.io>
2026-08-25 16:30:49 -0400

    signature

--- data/company.ts
+++ data/company.ts
@@ -6,22 +6,5 @@ export const company = {
   cage: '22QH1',
   email: 'james@beaglabs.com',
   url: 'https://www.beaglabs.com',
-  address: {
-    line1: '1717 Pennsylvania Ave NW',
-    line2: 'Suite 1025',
-    city: 'Washington',
-    state: 'DC',
-    zip: '20006',
-  },
-  certs: [
-    {
-      label: 'HUBZone Certified',
-      src: 'https://www.wildflowerintl.com/wp-content/uploads/2024/02/HubZoneCertified.png',
-    },
-    {
-      label: 'SBA',
-      src: 'https://images.seeklogo.com/logo-png/33/2/small-business-administration-logo-png_seeklogo-331699.png',
-    },
-  ],
   logoMark: 'B_',
 } as const
